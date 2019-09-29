3b8c4304573e67faa8a4292bf33121807f2809c6
Paul M <paulmcadory@gmail.com>
2019-09-29 18:49:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,8 @@
-var meals = {
-  breakfast: "oatmeal"
-};
-
-var meals = new
-Object({ breakfast: 'oatmeal' })
 var playlist = {
   Sophie: "Immaterial",
   Sandy: "Walk Away",
   Madonna: "Hung Up"
 };
+function updatePlaylist(playlist, ) {
+  
+}
