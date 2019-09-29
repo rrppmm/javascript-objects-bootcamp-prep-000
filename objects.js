2509d6723659d0cf9d69ecfe6d70d9658a496426
Paul M <paulmcadory@gmail.com>
2019-09-29 18:54:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,8 @@ var playlist = {
   '(Sandy) Alex G': 'Walk Away',
   'Madonna': 'Hung Up'
 };
-function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] = songTitle
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return 
   
 }
