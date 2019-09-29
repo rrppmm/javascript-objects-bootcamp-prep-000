596d41806a4bab28052dffc1dd6c1c28e77dc675
Paul M <paulmcadory@gmail.com>
2019-09-29 18:55:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playList;
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return
+  delete playlist[artistName];
+  return playlist;
 }
